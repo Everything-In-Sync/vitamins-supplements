319ae19b6eb3a5bc0268c27468b088061570abe5
everything-in-sync <robkososki@me.com>
2021-12-19 20:07:15 -0500

    added more vitamins

--- assets/js/vitamins.js
+++ assets/js/vitamins.js
@@ -36,12 +36,56 @@ let vitamins = [
         perDay: `Male: 90mg <br> Female: 75mg`,
         source: `<a href="https://ods.od.nih.gov/factsheets/VitaminC-HealthProfessional/" target="_blank">National Institute of Health</a>`,
     },
+
     {
         name: 'D',
-        food: 'Fish',
-        benefits: `Healthy Immune System, healthy skin <br> something else, something else`,
-        perDay: '5000 iu',
-        source: `<a href="" target="_blank"></a>`,
+        food: `Fortified Milk (1 cup) 120 IU <br>
+        Salmon (3.5 ounces) 526 IU<br>
+        Egg Yolk (1): 37 IU <br>
+        
+        `,
+
+        benefits: `Calcium Absorption, Immune Health, Muscle & Nerve Health`,
+        perDay: `600 IU`,
+        source: `<a href="https://ods.od.nih.gov/factsheets/VitaminD-Consumer/" target="_blank">National Institute of Health</a>`,
+    },
+    {
+        name: 'B12',
+        food: `Clams (Cooked 3 ounces): 84.1 mcg <br>
+        Lean Beef (Cooked 3 ounces): 6.9 mcg <br>
+        Milk (1 cup): 1.2 mcg <br>
+
+        <div class="box">
+            <a class="button" href="#popup1">More</a>
+        </div>
+    
+    <div id="popup1" class="overlay">
+        <div class="popup">
+
+            <a class="close" href="#">&times;</a>
+            <div class="popup-content">
+            Beef Liver (Cooked 3 ounces): 70.7 mcg<br>
+            Muscles (Steamed 3 ounces): 20.4 mcg<br>
+            King Crab (Steamed 3 ounces): 9.8 mcg<br>
+            Salmon (Cooked 3 ounces): 2.4 mcg<br>
+                Yogurt (8 ounces): 1.2 mcg<br>
+                Swiss Cheese (1 ounce): .9 mcg<br>
+
+
+
+            </div>
+        </div>
+    </div>
+        
+        `,
+
+        benefits: `Blood & Nerve Cell Health, Helps Make DNA`,
+        perDay: `2.4 mcg`,
+        source: `<a href="https://ods.od.nih.gov/factsheets/VitaminB12-Consumer/" target="_blank">National Institute of Health</a>
+        <br>
+        <a href="hhttps://www.webmd.com/diet/b12-rich-foods#1" target="_blank">Web MD</a>
+        
+        `,
     },
 
 ]
